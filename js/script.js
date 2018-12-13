1e29760628a9780802dc31f3435641964761d315
Pawel Oczkowski <pawel.oczko84@gmail.com>
2018-12-13 14:49:29 +0100

    Fix styles

--- js/script.js
+++ js/script.js
@@ -16,13 +16,3 @@ var btn1 = new Button('HELLO!');
 btn1.create();
 
 
-document.body.style.background = "#2a0a82";
-document.body.style.display = "flex";
-document.body.style.justifyContent = "center";
-var btn = document.querySelector('button');
-btn.style.backgroundColor = "gold";
-btn.style.fontSize = "25px";
-btn.style.color = "white";
-btn.style.border = "none";
-btn.style.borderRadius = "5px";
-btn.style.marginTop = "50px";
